2a3af7692a4ece1816a2c7e5c3143c6e9bd861bb
morelucks <luckykamshak@gmail.com>
2024-12-30 23:24:23 +0100

    basic tyoes

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 "use strict";
-let nam = "lucky";
-let naem = 9;
-console.log(nam);
-console.log(naem);
+// basic types
+// let id:number = 5;
+let company = 'morelucks';
+let isblack = true;
+let x = true;
+let y = 89;
+let z = 'name';
+let ids = [2, 2, 3, 0];
+ids.push(39);
+ids.pop();
+console.log(ids);
+// let nam="lucky";
+// let naem=9; 
+// console.log(nam);
+// console.log(naem);
--- index.ts
+++ index.ts
@@ -1,4 +1,24 @@
-let nam="lucky";
-let naem=9;
-console.log(nam);
-console.log(naem);
+// basic types
+
+let id:number = 5;
+let company:string='morelucks';
+let isblack:boolean=true;
+let x:any=true;
+let y:any=89;
+let z:any='name';
+
+
+let ids:number[]= [2,2,3,0];
+ids.push(39);
+ids.pop();
+ids.pop();
+ids.pop();
+
+let arr:any[]=[3, 4, " this is new", true, ]
+
+   
+console.log(ids);
+// let nam="lucky";
+// let naem=9; 
+// console.log(nam);
+// console.log(naem); 
