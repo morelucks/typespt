e31847afd3a818e1d187023cef408da8fe8f7cc3
morelucks <luckykamshak@gmail.com>
2024-12-31 05:54:33 +0100

    tupple arrya, union, tupple

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 "use strict";
+// tuple
+let person = [2, 'brad', true];
+// tupple Array
+let employee;
+employee = [
+    [1, "lucky"],
+    [2, "me"],
+    [3, "i"]
+];
+// union
+let pid = 484;
+let ppid = "number four";
+ppid = 45;
+console.log(ppid);
 // basic types
 // let id:number = 5;
-let company = 'morelucks';
-let isblack = true;
-let x = true;
-let y = 89;
-let z = 'name';
-let ids = [2, 2, 3, 0];
-ids.push(39);
-ids.pop();
-console.log(ids);
+// let company:string='morelucks';
+// let isblack:boolean=true;
+// let x:any=true;
+// let y:any=89;
+// let z:any='name';
+// let ids:number[]= [2,2,3,0];
+// ids.push(39);
+// ids.pop();
+// ids.pop();
+// ids.pop();
+// let arr:any[]=[3, 4, " this is new", true, ]
+// console.log(ids);
 // let nam="lucky";
 // let naem=9; 
 // console.log(nam);
-// console.log(naem);
+// console.log(naem); 
--- index.ts
+++ index.ts
@@ -1,23 +1,41 @@
+// tuple
+let person:[number, string, boolean]=[ 2, 'brad', true];
+// tupple Array
+let employee:[number, string][];
+employee=[
+    [1, "lucky"],
+    [2, "me"],
+    [3, "i"]
+]
+
+// union
+
+let pid:string|number= 484;
+let ppid:string|number="number four";
+ppid=45;
+console.log(ppid);
+
 // basic types
 
-let id:number = 5;
-let company:string='morelucks';
-let isblack:boolean=true;
-let x:any=true;
-let y:any=89;
-let z:any='name';
+
+// let id:number = 5;
+// let company:string='morelucks';
+// let isblack:boolean=true;
+// let x:any=true;
+// let y:any=89;
+// let z:any='name';
 
 
-let ids:number[]= [2,2,3,0];
-ids.push(39);
-ids.pop();
-ids.pop();
-ids.pop();
+// let ids:number[]= [2,2,3,0];
+// ids.push(39);
+// ids.pop();
+// ids.pop();
+// ids.pop();
 
-let arr:any[]=[3, 4, " this is new", true, ]
+// let arr:any[]=[3, 4, " this is new", true, ]
 
    
-console.log(ids);
+// console.log(ids);
 // let nam="lucky";
 // let naem=9; 
 // console.log(nam);
